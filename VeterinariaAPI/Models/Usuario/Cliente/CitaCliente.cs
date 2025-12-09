012776b9dd6d08bcafaa004c73ccec3bb45b234c
Miguel Luna <mike123de@gmail.com>
2025-12-09 02:18:30 -0500

    Descripción de tus cambios

---
+++ VeterinariaAPI/Models/Usuario/Cliente/CitaCliente.cs
@@ -0,0 +1,13 @@
+﻿namespace VeterinariaAPI.Models.Usuario.Cliente;
+
+public class CitaCliente
+{
+    public long ide_cit { get; set; }
+    public DateTime cal_cit { get; set; }
+    public int con_cit { get; set; }
+    public string veterinario { get; set; } 
+    public string especialidad { get; set; }
+    public string mascota { get; set; }    
+    public string especie { get; set; }   
+    public decimal mon_pag { get; set; }
+}
---
+++ VeterinariaAPI/Repository/DAO/EspecialidadDAO.cs
@@ -0,0 +1,36 @@
+using System.Data;
+using System.Data.SqlClient;
+using VeterinariaAPI.Models.Usuario.Veterinario; 
+using VeterinariaAPI.Repository.Interfaces;
+
+namespace VeterinariaAPI.Repository.DAO;
+
+public class EspecialidadDAO : IEspecialidad
+{
+    private readonly string _connectionString;
+
+    public EspecialidadDAO()
+    {
+        _connectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+            .Build().GetConnectionString("cn") ?? throw new NullReferenceException();
+    }
+
+    public IEnumerable<Especialidad> listarEspecialidad()
+    {
+        List<Especialidad> listaEspecialidad = new List<Especialidad>();
+        using var cn = new SqlConnection(_connectionString);
+        using var cmd = new SqlCommand("sp_listarEspecialidad", cn);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cn.Open();
+        using var dr = cmd.ExecuteReader();
+        while (dr.Read())
+        {
+            listaEspecialidad.Add(new Especialidad
+            {
+                ide_esp = Convert.ToInt64(dr[0]),
+                nom_esp = dr[1].ToString(),
+            });
+        }
+        return listaEspecialidad;
+    }
+}
